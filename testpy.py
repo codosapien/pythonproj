366918d3fdd7ce2848b25aa21b52b3ff6332f09a
codosapien <dummysapien@gmail.com>
2018-06-06 18:07:27 -0500

    testing commit

--- testpy.py
+++ testpy.py
@@ -1,4 +1,9 @@
 
-a=10
-b=20
-print a+b+10
+a= int(input("Enter the number for which the table is required: "))
+b= int(input("Enter the number for which the table is required: "))
+#b= int(input("Enter the end unit :"))
+#for i in range(1,b+1):
+ #   print a,"X",i,"=",a*i
+c=a+b
+print c
+print a+b
